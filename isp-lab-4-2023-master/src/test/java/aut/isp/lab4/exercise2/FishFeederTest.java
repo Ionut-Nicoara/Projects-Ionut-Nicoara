ce94ae800f8fc99818c05673d3b166e86c6efe9c
Nicoara-Ioan_30127 <ionutnicoara908@yahoo.com>
2023-09-06 23:51:46 +0300

    Implementation for the test class-exercise 2

--- isp-lab-4-2023-master/src/test/java/aut/isp/lab4/exercise2/FishFeederTest.java
+++ isp-lab-4-2023-master/src/test/java/aut/isp/lab4/exercise2/FishFeederTest.java
@@ -2,6 +2,40 @@ package aut.isp.lab4.exercise2;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class FishFeederTest {
+
+    @Test
+
+    public void feedTest(){
+
+        FishFeeder F1 = new FishFeeder("FishMax","V7");
+        F1.feed();
+        F1.feed();
+        assertEquals(12, F1.getMeals());
+
+    }
+
+    @Test
+
+    public void toStringTest(){
+
+        FishFeeder F1 = new FishFeeder("FishMax","V7");
+        assertEquals("Fish feeder FishMax V7", F1.toString());
+    }
+
+    @Test
+
+    public void fillUpTest(){
+
+        FishFeeder F1 = new FishFeeder("FishMax","V7");
+        F1.feed();
+        F1.feed();
+        F1.feed();
+        F1.fillUp();
+        assertEquals(14,F1.getMeals());
+    }
+
 
 }
